34fb29d40e02324046556306b2bde4bff3e82de5
Carolina Ochoa <cocho011@fiu.edu>
2024-05-27 16:54:30 -0500

    Modified config.js

--- employee-management-system/config.js
+++ employee-management-system/config.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'your_mysql_username',
-    password: 'your_mysql_password',
+    user: 'root',
+    password: 'Bluedot$8712',
     database: 'mysqldb'
 });
 
